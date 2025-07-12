6decf4d1db90ad302f67d1cad058ef0f82c44f23
MasonicClient17 <masonicclient17@gmail.com>
2025-07-12 15:06:26 +0500

    Add files via upload

---
+++ bot/main.py
@@ -0,0 +1,5 @@
+
+from pytgcalls import PyTgCalls
+
+def run_bot():
+    print("Running bot with embedded pytgcalls")
---
+++ bot/pytgcalls/__init__.py
@@ -0,0 +1,4 @@
+
+class PyTgCalls:
+    def __init__(self):
+        print("PyTgCalls initialized (local version)")
